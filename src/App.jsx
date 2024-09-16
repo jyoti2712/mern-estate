913838bbf7fccbbfeacd02b80627197969e563dd
jyoti2712 <jyotipanda2722@gmail.com>
2024-09-16 12:36:14 +0530

    header page

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/sign-in' element={<Signin />}/>
-      <Route path='/sign-out' element={<SignUp />}/>
-      <Route path='/about' element={<About />}/>
-      <Route path='/Profile' element={<Profile />}/>
-    </Routes>
-  </BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-out' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/Profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
