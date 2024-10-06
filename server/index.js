7b5e7c720ee54b429bab2695f0b7cd35eb113870
jyoti2712 <jyotipanda2722@gmail.com>
2024-10-06 13:18:24 +0530

    update user api route

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from './routers/user.route.js'
 import authRouter from './routers/auth.route.js';
+import cookieParser from "cookie-parser";
+import { verifyToken } from "./utils/verify.User.js";
 
 dotenv.config();
 
@@ -12,12 +15,14 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
 });
 
+app.use('/server/user', userRouter);
 app.use('/server/auth', authRouter)
 
 app.use((err, req, res, next) => {
